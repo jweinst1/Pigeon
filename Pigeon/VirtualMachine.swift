c0014e09d90cc31f0869f897d0f6e799a80b5330
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-06-06 00:11:09 -0700

    changes

---
+++ Pigeon/VirtualMachine.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualMachine.swift
+//  Pigeon
+//
+//  Created by Joshua Weinstein on 6/6/16.
+//  Copyright © 2016 Joshua Weinstein. All rights reserved.
+//
+
+import Foundation
+
+//main file for virtual machine
+
+class VirtualMachine {
+    var oper:String?
+    var r1:PigWrapper?
+    var r2:PigWrapper?
+}
